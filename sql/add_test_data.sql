63a23d3e82d18df1b25b9f60ec181194d53dc301
StargazerSalvation <markus.pahkamaa@helsinki.fi>
2015-04-10 13:36:00 -0700

    Testi datan lisäystä
    Päivitetty testi datan luonti paremmalle tasolle, nyt annetaan myös id:t drinkit_aineet -relaatiolle. Tapahtuu select lauseilla insertin sisällä.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -2,6 +2,11 @@
 INSERT INTO kayttaja (nimi, salasana, sposti, yllapitaja) VALUES('markus','markus123','diipadaapa@asa.as', true);
 INSERT INTO kayttaja (nimi, salasana, sposti) VALUES('kk','kk1234','ljaefa.etoih@asd.ty');
 
+INSERT INTO drinkit (nimi, kuvaus, ohje, ajankohta, makeus, lasi, lampotila, menetelma, ehdottaja_id, hyvaksytty_ehdotus) 
+    VALUES('Long island ice tea', 'DIIPA DAAPAA LONG ISLAND BLAA', 
+'Laita ravistimeen jäitä, lisää muut ainesosat. ja sekoita pikaisesti. Kaada juoma lasiin, jossa on jääpaloja pohjalla. Valmiin juoman tulisi poreilla kevyesti. Koristele juoma sitruunaviipaleella.',
+'All day', 'Makeahko', 'Collins-lasi', 'Jäinen', 'Ravistetaan', 1, true);
+
 INSERT INTO ainesosat (nimi, tyyppi) VALUES('Gin', 'Väkevä viina');
 INSERT INTO ainesosat (nimi, tyyppi) VALUES('Rommi (vaalea)', 'Väkevä viina');
 INSERT INTO ainesosat (nimi, tyyppi) VALUES('Cola limsa', 'Mikseri');
@@ -10,12 +15,43 @@ INSERT INTO ainesosat (nimi, tyyppi) VALUES('Triple sec', 'Likööri');
 INSERT INTO ainesosat (nimi, tyyppi) VALUES('Tequila', 'Väkevä viina');
 INSERT INTO ainesosat (nimi, tyyppi) VALUES('Vodka', 'Väkevä viina');
 
-INSERT INTO drinkit (nimi, kuvaus, ohje, ajankohta, makeus, lasi, lampotila, menetelma, ehdottaja_id, hyvaksytty_ehdotus) 
-VALUES('Long island ice tea', 'DIIPA DAAPAA LONG ISLAND BLAA', 
-'Laita ravistimeen jäitä, lisää muut ainesosat. ja sekoita pikaisesti. Kaada juoma lasiin, jossa on jääpaloja pohjalla. Valmiin juoman tulisi poreilla kevyesti. Koristele juoma sitruunaviipaleella.',
-'All day', 'Makeahko', 'Collins-lasi', 'Jäinen', 'Ravistetaan', 1, true);
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Long island ice tea'), 
+    (select id from ainesosat where nimi = 'Vodka'), '1 cl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Long island ice tea'), 
+    (select id from ainesosat where nimi = 'Gin'), '1 cl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Long island ice tea'), 
+    (select id from ainesosat where nimi = 'Rommi (vaalea)'), '1 cl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Long island ice tea'), 
+    (select id from ainesosat where nimi = 'Cola limsa'), '1 dl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Long island ice tea'), 
+    (select id from ainesosat where nimi = 'Siirappi (sitruuna)'), '1,5 cl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Long island ice tea'), 
+    (select id from ainesosat where nimi = 'Triple sec'), '1 cl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Long island ice tea'), 
+    (select id from ainesosat where nimi = 'Tequila'), '1 cl');
 
 INSERT INTO drinkit (nimi, kuvaus, ohje, ajankohta, makeus, lasi, lampotila, menetelma, ehdottaja_id, hyvaksytty_ehdotus) 
-VALUES('Valkovenäläinen', 'kahvinen maku ei mikään ryyppäämis juoma', 
-'kaadetaan pohjalle ural mocca likööriä 2cl ja maitoa 4cl seotetaan voi lisätä kerma vaahtoa',
-'All day', 'Makea', 'Cocktail-lasi', 'Viileä', 'Rakennetaan', 1, true);
+    VALUES('Valkovenäläinen', 'kahvinen maku ei mikään ryyppäämis juoma', 
+    'kaadetaan pohjalle ural mocca likööriä 2cl ja maitoa 4cl seotetaan voi lisätä kerma vaahtoa',
+    'All day', 'Makea', 'Cocktail-lasi', 'Viileä', 'Rakennetaan', 1, true);
+
+INSERT INTO ainesosat (nimi, tyyppi) VALUES('Kerma', 'Mikseri');
+INSERT INTO ainesosat (nimi, tyyppi) VALUES('Kahlua', 'Likööri');
+
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Valkovenäläinen'), 
+    (select id from ainesosat where nimi = 'Kerma'), '2 cl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Valkovenäläinen'), 
+    (select id from ainesosat where nimi = 'Kahlua'), '2 cl');
+INSERT INTO Drinkki_aineet (drinkki_id, aine_id, maara) 
+    VALUES((select id from drinkit where nimi = 'Valkovenäläinen'), 
+    (select id from ainesosat where nimi = 'Vodka'), '2 cl');
+
